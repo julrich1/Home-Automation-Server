499e463ddb23f139cfbf75b42b7dc01bd26e826b
Jacob Ulrich <julrich@gmail.com>
2018-01-21 20:57:39 -0800

    Fixed commands for volume

--- routes/volume.js
+++ routes/volume.js
@@ -5,11 +5,12 @@ const { exec } = require('child_process');
 const getChannelList = require("../actions/twitch-channel-list");
 
 router.get("/volumeup", (req, res, next) => {
-  exec("node ./tools/harmonyHubCLI/harmonyHubCli.js  -l 192.168.1.198 -d 'LG Amp' -c '[\"VolumeUp\", \"VolumeUp\"]'");
+  exec("node tools/harmonyHubCLI/harmonyHubCli.js  -l 192.168.1.198 -d 'LG Amp' -c '[\"VolumeUp\", \"VolumeUp\"]' -m");
   res.send("Done"); 
 });
 
 router.get("/volumedown", (req, res, next) => {
+  exec("node tools/harmonyHubCLI/harmonyHubCli.js  -l 192.168.1.198 -d 'LG Amp' -c '[\"VolumeDown\", \"VolumeDown\"]' -m");
   res.send("Done"); 
 });
 
