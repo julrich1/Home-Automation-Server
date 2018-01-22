ee85937ddfc55cc7d0293ccd930bbd9d312b3063
Jacob Ulrich <julrich@gmail.com>
2018-01-21 19:47:44 -0800

    Default to clearing sleep timers if they exist

--- routes/goodnight.js
+++ routes/goodnight.js
@@ -7,15 +7,20 @@ const router = express.Router();
 let dimTimer = null;
 
 router.get("/goodnight", (req, res, next) => {
+  if (dimTimer !== null) {
+    console.log("Clearing the dim timer");
+    clearInterval(dimTimer);
+  }
+
   noiseMachine(true);
   dimTimer = dimLights();
   res.send("Goodnight"); 
 });
 
-router.get("/goodnight/stop", (req, res, next) => {
-  noiseMachine(false);
-  clearInterval(dimTimer);
-  res.send("Stop");
-});
+// router.get("/goodnight/stop", (req, res, next) => {
+//   noiseMachine(false);
+//   clearInterval(dimTimer);
+//   res.send("Stop");
+// });
 
 module.exports = router;
