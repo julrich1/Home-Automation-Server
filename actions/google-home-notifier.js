394aec1e5039ab18353a7a16f67266045a15f37c
Jacob Ulrich <julrich@gmail.com>
2019-04-21 14:40:44 -0700

    Switched to fixed IP address for ghome

--- actions/google-home-notifier.js
+++ actions/google-home-notifier.js
@@ -1,7 +1,7 @@
 function notify(message) {
   const googlehome = require("google-home-notifier");
 
-  googlehome.device("Living room Home");
+  googlehome.device("192.168.86.32");
   googlehome.notify(message, function(res) {
     console.log(`Sent Notification: ${message}`);
     // console.log(res);
