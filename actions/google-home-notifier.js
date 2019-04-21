1e1d60d03585a6a429694559722c886dd5aac677
Jacob Ulrich <julrich@gmail.com>
2019-04-21 14:38:43 -0700

    Fixed missing quotation mark

--- actions/google-home-notifier.js
+++ actions/google-home-notifier.js
@@ -1,7 +1,7 @@
 function notify(message) {
   const googlehome = require("google-home-notifier");
 
-  googlehome.device(Living room Home");
+  googlehome.device("Living room Home");
   googlehome.notify(message, function(res) {
     console.log(`Sent Notification: ${message}`);
     // console.log(res);
